6f720844c15fc21f4190ab5157a7c2fc3256ba0b
Junaid Ahmad <32243638+juac08@users.noreply.github.com>
2021-01-12 09:54:06 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.random();
+var randomNumber1= (Math.floor(randomNumber1*6)+1);
+var randomsource= "dice" + randomNumber1 + ".png";
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute ("src" , randomsource);
+
+
+
+var randomNumber2 = Math.random();
+var randomNumber2= (Math.floor(randomNumber2*6)+1);
+var randomsource2= "dice" + randomNumber2 + ".png";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute ("src" , randomsource2);
+
+if(randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML="Player 1 Won";
+}
+else if(randomNumber2 > randomNumber1){
+
+document.querySelector("h1").innerHTML=" Player 2 Won";
+}
+else {
+  document.querySelector("h1").innerHTML=" 🏁Match Draw";
+}
