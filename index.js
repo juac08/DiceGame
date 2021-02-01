8253ca57a430bdce90a24a37502b9718a26d3fde
Junaid Ahmad <32243638+juac08@users.noreply.github.com>
2021-02-01 09:04:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.random();
 var randomNumber1= (Math.floor(randomNumber1*6)+1);
-var randomsource= "/dice" + randomNumber1 + ".png";
+var randomsource= "dice" + randomNumber1 + ".png";
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute ("src" , randomsource);
 
@@ -8,7 +8,7 @@ image1.setAttribute ("src" , randomsource);
 
 var randomNumber2 = Math.random();
 var randomNumber2= (Math.floor(randomNumber2*6)+1);
-var randomsource2= "/dice" + randomNumber2 + ".png";
+var randomsource2= "dice" + randomNumber2 + ".png";
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute ("src" , randomsource2);
 
