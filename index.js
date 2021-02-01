daa74a09cd6798d5cdd178180e52bf2de1a15fd8
Junaid Ahmad <32243638+juac08@users.noreply.github.com>
2021-02-01 10:29:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ image1.setAttribute ("src" , randomsource);
 
 
 var randomNumber2 = Math.random();
-var randomNumber2= (Math.floor(randomNumber2*6)+1);
+var randomNumber2= (Math.floor(randomNumber2*6 | 0)+1);
 var randomsource2= "./dice" + randomNumber2 + ".png";
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute ("src" , randomsource2);
@@ -18,9 +18,6 @@ if(randomNumber1 > randomNumber2) {
 else if(randomNumber2 > randomNumber1){
 
 document.querySelector("h1").innerHTML=" Player 2 Won";
-}
-else if(randomNumber1 || randomNumber2 ===0){
-    document.querySelector("h1").innerHTML=" 🏁Match Draw";
 }
   
 else {
