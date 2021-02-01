6ad87fc80d04a5e4359472412c3f63c26b1b6d3b
Junaid Ahmad <32243638+juac08@users.noreply.github.com>
2021-02-01 10:19:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.random();
 var randomNumber1= (Math.floor(randomNumber1*6)+1);
 var randomsource= "./dice" + randomNumber1 + ".png";
 var image1=document.querySelectorAll("img")[0];
-image1.setAttribute ("src" , randomsource);
+image1.setAttribute ("./src" , randomsource);
 
 
 
@@ -10,7 +10,7 @@ var randomNumber2 = Math.random();
 var randomNumber2= (Math.floor(randomNumber2*6)+1);
 var randomsource2= "./dice" + randomNumber2 + ".png";
 var image2=document.querySelectorAll("img")[1];
-image2.setAttribute ("src" , randomsource2);
+image2.setAttribute ("./src" , randomsource2);
 
 if(randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML="Player 1 Won";
